661613ef1dbb96b86c1ee3de1e7547f7248b81da
Romain Amirian <xunt1tledx1@gmail.com>
2019-02-27 01:09:54 +0400

    Let me know if something else is required

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,34 @@
+jQuery(document).ready(function ($) {
+	$('.mobile-switcher').on('click', function (ev) {
+		if (!$('.nav-menu').hasClass('active')) {
+			$('.nav-menu').addClass('active');
+		} else {
+			$('.nav-menu').addClass('unactive');
+
+			setTimeout(function () {
+				$('.nav-menu').removeClass('active');
+				$('.nav-menu').removeClass('unactive');
+			}, 500);
+		}
+	});
+
+	$('.choice-menu-item').on('click', function (ev) {
+		ev.preventDefault();
+		if (!$(this).hasClass('selected-choice')) {
+			$('.choice-menu-item').removeClass('selected-choice');
+			$(this).addClass('selected-choice');
+		}
+	});
+
+	$('.cars-list-item').on('click', function (ev) {
+		ev.preventDefault();
+		if (!$(this).hasClass('selected-car-item')) {
+			$('.cars-list-item').removeClass('selected-car-item');
+			$(this).addClass('selected-car-item');
+		}
+	});
+});
+
 /*
 $(window).scroll(function () {
 	var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
